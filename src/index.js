dcceb1946487d9bff831d317449bbe28f75de3f7
Pau Ramon Revilla <masylum@gmail.com>
2017-06-23 17:55:39 +0200

    Prettier pixelgs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import pixelUrl from './pixelUrl'
 
 const img = document.createElement('img')
 img.src = pixelUrl(document)
+img.width = 1
+img.height = 1
+img.style = "border-style:none;"
 document.body.appendChild(img)
