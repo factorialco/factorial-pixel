b4d67e3407a191bbed3291ccdbb4d67c35087ab8
Pau Ramon Revilla <masylum@gmail.com>
2017-06-23 12:34:58 +0200

    relative url

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## `0.2.1`
+
+Fixed relative url
+
 ## `0.2.0`
 
 Now the pixel is a real pixel.
--- src/pixelUrl.js
+++ src/pixelUrl.js
@@ -10,5 +10,5 @@ export default (document) => {
     `landing_page=${landingPage}`
   ].join('&')
 
-  return `https://factorialhr.com/api/internal/pixel?${attributes}`
+  return `/api/internal/pixel?${attributes}`
 }
