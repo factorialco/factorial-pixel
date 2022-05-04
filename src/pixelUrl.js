4492fdec94d6eb53006bcc00a9d7e9ecc588d942
SergioMD15 <sergiomosquera96@gmail.com>
2022-05-04 15:44:06 +0200

    Get Google Click ID from URL and send to pixel endpoint

--- src/pixelUrl.js
+++ src/pixelUrl.js
@@ -1,13 +1,14 @@
 import requestParameters from './requestParameters'
 
 export default (document) => {
-  const { language, landingPage } = requestParameters(document)
+  const { language, landingPage, gclid } = requestParameters(document)
   const mc = document.location.href.match(/mc=(.*)/)
   const attributes = [
     `mc=${mc ? mc[1] : ''}`,
     `referer=${encodeURI(document.referrer)}`,
     `language=${language}`,
-    `landing_page=${landingPage}`
+    `landing_page=${landingPage}`,
+    `gclid=${gclid}`,
   ].join('&')
 
   return `/internal/pixel?${attributes}`
--- src/requestParameters.js
+++ src/requestParameters.js
@@ -1,9 +1,20 @@
-export default function requestParameters (document) {
-  const landing = encodeURI(document.location.href.split('?')[0])
+function findPropertyInParams(params, property) {
+  params.split('&').reduce((acc, param) => {
+    const [key, value] = param.split('=')
+
+    return { ...acc, [key]: value }
+  }, {})[property]
+}
+
+export default function requestParameters(document) {
+  const [path, params] = document.location.href.split('?')
+  const landing = encodeURI(path)
   const locale = document.querySelector('html').lang.split('-')[0]
+  const gclid = findPropertyInParams(params, 'gclid')
 
   return {
     language: locale,
-    landingPage: landing
+    landingPage: landing,
+    gclid,
   }
 }
